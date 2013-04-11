98500122409e7f5ba0fbda14aff8589388c4250c
thilohaas <thilo@thilohaas.ch>
2013-04-11 17:39:37 +0200

    answer validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,10 @@ class Answer < ActiveRecord::Base
   attr_accessible :answer, :description, :question, :rating, :user, :user_id
   belongs_to :question
   belongs_to :user
+  validates_format_of :answer, :with => /^[a-z]+$/i
+
+  before_create :upcase_answer
+  def upcase_answer
+     self.answer.upcase! if self.answer
+  end
 end
